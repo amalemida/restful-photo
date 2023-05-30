68645253cbda8c2eb80e87926f251ad0b65bfafe
aMarceloAlmeida <amalemida@gmail.com>
2023-05-29 21:20:17 -0300

    ajuste

--- bdconfig.js
+++ bdconfig.js
@@ -1,20 +1,10 @@
 module.exports = {
-  // crie uma variável de ambiente no Windows chamada
-  // NODE_MYSQL_SERVIDOR e faça ela valer o nome ou o
-  // endereço IP da máquina que executa o MySQL (se for
-  // uma instalação local, faça ela valer LOCALHOST)
+  
   host: 'us-cdbr-east-06.cleardb.net',
 
-  // crie uma variável de ambiente no Windows chamada
-  // NODE_MYSQL_USUARIO e faça ela valer o seu login
   user: 'b9c9cc3e2a620d',
 
-  // crie uma variável de ambiente no Windows chamada
-  // NODE_MYSQL_SENHA e faça ela valer sua senha
   password: 'e67c3614',
 
-  // crie uma variável de ambiente no Windows chamada
-  // NODE_MYSQL_BD e faça ela valer nome do BD que
-  // pretende acessar
   database: 'heroku_3d1f304c7e13b30'
 };
