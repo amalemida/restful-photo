c2eba1c4139783352227d0548bb8d05053b78d72
aMarceloAlmeida <amalemida@gmail.com>
2023-05-31 21:04:57 -0300

    ajuste banco

--- bdconfig.js
+++ bdconfig.js
@@ -1,10 +1,18 @@
 module.exports = {
  
-  host: 'us-cdbr-east-06.cleardb.net',
+  // host: 'us-cdbr-east-06.cleardb.net',
 
-  user: 'b9c9cc3e2a620d',
+  // user: 'b9c9cc3e2a620d',
 
-  password: 'e67c3614',
+  // password: 'e67c3614',
 
-  database: 'heroku_3d1f304c7e13b30'
+  // database: 'heroku_3d1f304c7e13b30'
+
+  host: 'regulus.cotuca.unicamp.br',
+
+  user: 'BD22529',
+
+  password: 'BD22529',
+
+  database: 'BD22529'
 };
